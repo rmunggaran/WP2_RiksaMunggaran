7cd1923a5d55def4ed1a369b8a8eb0c1b4ef2272
rmunggaran <riksamunggaran252@gmail.com>
2023-09-25 17:35:41 +0700

    Perbarui submodule ke commit terbaru

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,8 @@
+<?php 
+class Contoh1 extends CI_Controller{
+    public function index(){
+        echo"<h1>Perkenalan</h1>";
+        echo"Nama saya Riksa Munggaran saya dari manonjaya tasikmalaya saya sedang belajar ngoding";
+    }
+}
+?>
