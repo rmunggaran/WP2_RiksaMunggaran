5bfea0adbcd5bf88ed09c0ddc033a5b80971709d
rmunggaran <riksamunggaran252@gmail.com>
2023-09-25 18:52:06 +0700

    menambah Codeigniter

--- application/controllers/contoh1.php
+++ application/controllers/contoh1.php
@@ -2,7 +2,7 @@
 class Contoh1 extends CI_Controller{
     public function index(){
         echo"<h1>Perkenalan</h1>";
-        echo"Nama saya Riksa Munggaran saya dari manonjaya tasikmalaya saya sedang belajar";
+        echo"Nama saya Riksa Munggaran saya dari manonjaya tasikmalaya saya sedang belajar Codeigniter";
     }
 }
 ?>
