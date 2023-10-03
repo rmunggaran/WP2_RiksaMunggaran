42b93dfafba6f469e495f0a06e2c68bc4b2d4a79
rmunggaran <riksamunggaran252@gmail.com>
2023-10-03 18:10:43 +0700

    menambahkan latihan

---
+++ latihan_ci/application/views/conroh.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>latihan pertama</title>
+</head>
+<body>
+    <p>Latihan saya belajar frarework CI</p>
+    <p>frarework CI sangat mudah di pelajari</p>
+</body>
+</html>
