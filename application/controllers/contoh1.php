3aa6cae03eb843eaa0d57b03001087084d8dbc80
rmunggaran <riksamunggaran252@gmail.com>
2023-10-03 18:08:56 +0700

    menghapus file yang tidak berhuna
    p

--- application/controllers/contoh1.php
+++
@@ -1,8 +0,0 @@
-<?php 
-class Contoh1 extends CI_Controller{
-    public function index(){
-        echo"<h1>Perkenalan</h1>";
-        echo"Nama saya Riksa Munggaran saya dari manonjaya tasikmalaya saya sedang belajar Codeigniter untuk pemula";
-    }
-}
-?>
