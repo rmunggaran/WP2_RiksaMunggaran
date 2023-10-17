56b836f32f7a53e0d92010cacc3383f122b8f835
rmunggaran <riksamunggaran252@gmail.com>
2023-10-17 16:38:34 +0700

    menambah DB baru

--- crud_ci/database/db_kampus.sql
+++ crud_ci/database/db_kampus.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 03 Okt 2023 pada 11.49
+-- Waktu pembuatan: 17 Okt 2023 pada 11.37
 -- Versi server: 10.4.28-MariaDB
 -- Versi PHP: 8.0.28
 
@@ -32,16 +32,19 @@ CREATE TABLE `mahasiswa` (
   `nim` varchar(8) NOT NULL,
   `nama` varchar(50) NOT NULL,
   `alamat` text NOT NULL,
-  `pekerjaan` varchar(50) NOT NULL
+  `pekerjaan` varchar(50) NOT NULL,
+  `foto` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data untuk tabel `mahasiswa`
 --
 
-INSERT INTO `mahasiswa` (`id`, `nim`, `nama`, `alamat`, `pekerjaan`) VALUES
-(1, '12220938', 'Azis Supriatna', 'jl.contoh no 1 desa contoh kec contoh kab.contoh', 'kdjksajdk'),
-(2, '12220808', 'jahroni', 'adwdwdwdADW', 'idioiwdq diuqdhuwqi');
+INSERT INTO `mahasiswa` (`id`, `nim`, `nama`, `alamat`, `pekerjaan`, `foto`) VALUES
+(1, '12220938', 'Azis Supriatna', 'jl.contoh no 1 desa contoh kec contoh kab.contoh', 'wirasuasta', 'images/resi-pengiriman.jpg'),
+(3, '12220808', 'himawari', 'jl.contoh no 1 desa contoh kec contoh kab.contoh', 'Penyan', 'images/JohnvonNeumann-LosAlamos.jpg'),
+(8, '12220808', 'jahroni', 'KP SUKAMULYA , RT 06 RW 02, DESA KAMULYAN', 'tukang sampah', 'images/6.png'),
+(15, '12220697', 'Azis Supriatna', 'jl.contoh no 1 desa contoh kec contoh kab.contoh', 'wirasuasta', 'images/1-b8799db8a5d9b07e175b26fb38e61ddd.jpg');
 
 --
 -- Indexes for dumped tables
@@ -61,7 +64,7 @@ ALTER TABLE `mahasiswa`
 -- AUTO_INCREMENT untuk tabel `mahasiswa`
 --
 ALTER TABLE `mahasiswa`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
