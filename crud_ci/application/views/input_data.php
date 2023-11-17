644022b58b74e33e05d3e436bbd6e6b644f24fa3
rmunggaran <riksamunggaran252@gmail.com>
2023-11-17 13:08:14 +0700

    men desain tampilan data dan tambah data

--- crud_ci/application/views/input_data.php
+++ crud_ci/application/views/input_data.php
@@ -4,42 +4,50 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Input Data mahasiswa</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
 </head>
 <body>
     <center>
-        <h1>Input Data mahasiswa</h1>
         <label style="color:#FF0000"><?php echo validation_errors(); ?></label>
     </center>
-    <center>
-    <form action="<?php echo base_url() . 'index.php/kampus/tambah_aksi'; ?>" method="post" enctype="multipart/form-data">
-    <table>
-        <tr>
-            <td>NIM</td>
-            <td><input type="text" name="nim"></td>
-        </tr>
-        <tr>
-            <td>Nama</td>
-            <td><input type="text" name="nama"></td>
-        </tr>
-        <tr>
-            <td>Alamat</td>
-            <td><input type="text" name="alamat"></td>
-        </tr>
-        <tr>
-            <td>Pekerjaan</td>
-            <td><input type="text" name="pekerjaan"></td>
-        </tr>
-        <tr>
-            <td>Upload Foto</td>
-            <td><input type="file" name="foto"></td>
-        </tr>
-        <tr>
-            <td></td>
-            <td><input type="submit" value="Tambah"><input type="reset" value="Batal"></td>
-        </tr>
-    </table>
-</form>
-</center>
+    <div class="container-fluid mt-5">
+        <div class="row h-100 align-items-center justify-content-center" style="min-height: 100vh;">
+            <div class="col-12 col-sm-8 col-md-6 col-lg-5 col-xl-4">
+                <div class="bg-light rounded p-4 p-sm-5 my-4 mx-3">
+                    <form action="<?php echo base_url() . 'index.php/kampus/tambah_aksi'; ?>" method="post" enctype="multipart/form-data">
+                        <div class="d-flex justify-content-between mb-3">
+                            <h3 class="text-primary"><i class="fa fa-hashtag me-2">Input Data mahasiswa</i></h3>
+                           
+                        </div>
+                        <div class="form-floating mb-3">
+                            <input type="text" class="form-control" id="floatingInput" placeholder="name@example.com" name="nim">
+                            <label for="floatingInput">NIM</label>
+                        </div>
+                        <div class="form-floating mb-3">
+                            <input type="text" class="form-control" id="floatingInput" placeholder="name@example.com" name="nama">
+                            <label for="floatingInput">Nama</label>
+                        </div>
+                        <div class="form-floating mb-3">
+                            <input type="text" class="form-control" id="floatingInput" placeholder="name@example.com" name="alamat">
+                            <label for="floatingInput">alamat</label>
+                        </div>
+                        <div class="form-floating mb-3">
+                            <input type="text" class="form-control" id="floatingInput" placeholder="name@example.com" name="pekerjaan">
+                            <label for="floatingInput">pekerjaan</label>
+                        </div>
+                            <input class="form-control mb-3" type="file" id="formFile" name="foto">
+                        <input type="submit" value="Tambah" class="btn btn-primary py-3 w-100 mb-4">
+                    </form>
+                    <a href="<?php echo base_url(); ?>/kampus" class="btn btn-primary">Kembali</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 
 </body>
 </html>
--- crud_ci/application/views/tampil_data.php
+++ crud_ci/application/views/tampil_data.php
@@ -4,37 +4,44 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tampilkan data</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?php echo base_url() ?>anu/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
 </head>
 <body>
-    <center><h1>Data mahasiswa</h1></center>
-    <table style="margin:20px auto;" border="1">
-        <tr>
-            <th>No</th>
-            <th>Nim</th>
-            <th>Nama</th>
-            <th>Alamat</th>
-            <th>Pekerjaan</th>
-            <th>Foto</th>
-            <th>action</th>
-        </tr>
-        <?php $no=1;
-        foreach($mahasiswa as $u){ ?>
-        <tr>
-            <td><?php echo $no++ ?></td>
-            <td><?php echo $u->nim ?></td>
-            <td><?php echo $u->nama ?></td>
-            <td><?php echo $u->alamat ?></td>
-            <td><?php echo $u->pekerjaan ?></td>
-            <td><img src="<?php echo base_url(); ?><?php echo $u->foto ?>" width="50" height="50"></td>
-            <td>
-                <?php echo anchor('kampus/edit/'.$u->id,'Edit') ?>
-                <?php echo anchor('kampus/hapus/'.$u->id,'Hapus') ?>
-            </td>
-        </tr>
-        
-        <?php } ?>
-    </table>
-    <center><?php echo anchor('kampus/tambah','<input type=button value="Tambah Data">'); ?></center>
+    <div class="container">
+        <center class="mt-5"><h1>Data mahasiswa</h1></center>
+        <table style="margin:20px auto;" class="table table-striped border p-2 mb-2">
+            <tr>
+                <th>No</th>
+                <th>Nim</th>
+                <th>Nama</th>
+                <th>Alamat</th>
+                <th>Pekerjaan</th>
+                <th>Foto</th>
+                <th>action</th>
+            </tr>
+            <?php $no=1;
+            foreach($mahasiswa as $u){ ?>
+            <tr>
+                <td><?php echo $no++ ?></td>
+                <td><?php echo $u->nim ?></td>
+                <td><?php echo $u->nama ?></td>
+                <td><?php echo $u->alamat ?></td>
+                <td><?php echo $u->pekerjaan ?></td>
+                <td><img src="<?php echo base_url(); ?><?php echo $u->foto ?>" width="50" height="50" class="rounded-circle"></td>
+                <td>
+                    <a href="<?php echo base_url(); ?>kampus/edit/<?= $u->id ?>/Edit" class="btn btn-primary"> <i class="bi bi-pencil-square"></i> Edit</a>
+                    <a href="<?php echo base_url(); ?>kampus/hapus/<?= $u->id ?>/Hapus" class="btn btn-danger"><i class="bi bi-trash-fill"></i> hapus</a>
+                </td>
+            </tr>
+            
+            <?php } ?>
+        </table>
+        <center><?php echo anchor('kampus/tambah','<input type=button value="Tambah Data">'); ?></center>
+
+    </div>
     
 </body>
 </html>
